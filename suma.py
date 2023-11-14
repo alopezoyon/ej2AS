43e1dd5db8a4c11331a163f9523128380a20fc1b
Adrián <74185568+alopezoyon@users.noreply.github.com>
2023-11-14 16:11:10 +0100

    Create suma.py

---
+++ suma.py
@@ -0,0 +1,11 @@
+# Función que suma dos números
+def suma_dos_numeros(num1, num2):
+    return num1 + num2
+
+# Dos variables con números (puedes cambiar estos valores)
+numero1 = 5
+numero2 = 7
+
+# Mostrar la suma de los dos números usando la función
+resultado_suma = suma_dos_numeros(numero1, numero2)
+print(f"La suma de {numero1} y {numero2} es: {resultado_suma}")
